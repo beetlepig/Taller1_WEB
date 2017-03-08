986e0027894ed7d6bcbd5f744115fb87c12199b7
karlos <sky_karlos@hotmail.com>
2017-03-08 15:15:10 -0500

    ...

--- public/js/DBproductos.js
+++ public/js/DBproductos.js
@@ -1,8 +1,13 @@
 
 var productos= [
-    {"nombre":"Rosa", "srcIMG":"../public/images/imgProductos/1.jpg", "categoria":"Flor"},
-    {"nombre":"Saman", "srcIMG":"../public/images/imgProductos/1.jpg", "categoria":"Arbol"},
+    {"nombre":"Rosa", "srcIMG":"/images/imgProductos/2.jpg", "categoria":"Flor", "color":"249,168,37 ,1"},
+    {"nombre":"Saman", "srcIMG":"/images/imgProductos/1.jpg", "categoria":"Arbol", "color":"229,57,53 ,1"},
+    {"nombre":"Helecho", "srcIMG":"/images/imgProductos/3.jpg", "categoria":"Helecho", "color":"109,76,65 ,1"}
 
 
 ];
 
+
+exports.getall= function () {
+    return productos;
+};
--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var productosDB= require('../public/js/DBproductos');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('home', { title: 'Jardin Misterioso' });
+    res.render('home', {title: 'Jardin Misterioso',productos:productosDB.getall()});
+
 });
 
 module.exports = router;
