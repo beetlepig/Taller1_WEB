42c39b832f5acab4404bb9fdf4335c93bd4e76d9
sky_k <sky_karlos@hotmail.com>
2017-03-11 17:06:11 -0500

    ...

--- public/js/DBproductos.js
+++ public/js/DBproductos.js
@@ -1,17 +1,135 @@
 
-var productos= [
-    {"nombre":"Rosa", "srcIMG":"/images/imgProductos/2.jpg", "categoria":"Flor", "color":"249,168,37 ,1"},
-    {"nombre":"Saman", "srcIMG":"/images/imgProductos/1.jpg", "categoria":"Arbol", "color":"229,57,53 ,1"},
-    {"nombre":"Helecho", "srcIMG":"/images/imgProductos/3.jpg", "categoria":"Helecho", "color":"109,76,65 ,1"},
-    {"nombre":"Dolar", "srcIMG":"/images/imgProductos/4.jpg", "categoria":"Arbusto o planta pequeña", "color":"3,155,229 ,1"},
-    {"nombre":"Tulipán", "srcIMG":"/images/imgProductos/5.jpg", "categoria":"Flor", "color":"249,168,37 ,1"},
-    {"nombre":"Cedro", "srcIMG":"/images/imgProductos/6.jpg", "categoria":"Arbol", "color":"229,57,53 ,1"},
-    {"nombre":"Culantrillo", "srcIMG":"/images/imgProductos/7.jpg", "categoria":"Helecho", "color":"109,76,65 ,1"},
-    {"nombre":"Nogal", "srcIMG":"/images/imgProductos/8.jpg", "categoria":"Arbol", "color":"229,57,53 ,1"}
+var productosFiltrados = [];
+var arbolCheck=false;
+var arbustoCheck=false;
+var florCheck=false;
+var helechoCheck;
 
+var productos= [
+    {"nombre":"Rosa", "srcIMG":"/images/imgProductos/2.jpg", "categoria":"Flor", "color":"249,168,37 ,1", "clima":"Frio"},
+    {"nombre":"Saman", "srcIMG":"/images/imgProductos/1.jpg", "categoria":"Arbol", "color":"229,57,53 ,1", "clima":"Caliente"},
+    {"nombre":"Helecho", "srcIMG":"/images/imgProductos/3.jpg", "categoria":"Helecho", "color":"109,76,65 ,1", "clima":"Templado"},
+    {"nombre":"Dolar", "srcIMG":"/images/imgProductos/4.jpg", "categoria":"Arbusto o planta pequeña", "color":"3,155,229 ,1", "clima":"Lluvioso"},
+    {"nombre":"Tulipán", "srcIMG":"/images/imgProductos/5.jpg", "categoria":"Flor", "color":"249,168,37 ,1", "clima":"Seco"},
+    {"nombre":"Cedro", "srcIMG":"/images/imgProductos/6.jpg", "categoria":"Arbol", "color":"229,57,53 ,1", "clima":"Frio"},
+    {"nombre":"Culantrillo", "srcIMG":"/images/imgProductos/7.jpg", "categoria":"Helecho", "color":"109,76,65 ,1","clima":"Caliente"},
+    {"nombre":"Nogal", "srcIMG":"/images/imgProductos/8.jpg", "categoria":"Arbol", "color":"229,57,53 ,1", "clima":"Templado"},
+    {"nombre":"Rocio de Oro", "srcIMG":"/images/imgProductos/9.jpg", "categoria":"Arbusto o planta pequeña", "color":"3,155,229 ,1","clima":"Lluvioso"},
+    {"nombre":"Aloe Vera", "srcIMG":"/images/imgProductos/10.jpg", "categoria":"Flor", "color":"249,168,37 ,1", "clima":"Seco"}
 ];
 
 
 exports.getall= function () {
     return productos;
+};
+
+exports.clearFiltroArray= function () {
+  productosFiltrados= [];
+};
+
+exports.getFiltro= function () {
+  return productosFiltrados;
+};
+
+exports.pushCategoriaFlor= function () {
+
+    for (i = 0; i < productos.length; i++) {
+        if (productos[i].color == "249,168,37 ,1") {
+            if (florCheck==false) {
+                productosFiltrados.push(productos[i]);
+
+            } else {
+                var index =productosFiltrados.indexOf(productos[i]);
+                if (index >= 0) {
+                    productosFiltrados.splice(index, 1);
+                }
+
+            }
+        }
+    }
+
+    if(!florCheck){
+        florCheck=true;
+    } else {
+        florCheck=false;
+    }
+
+};
+
+exports.pushCategoriaArbusto= function () {
+
+    for (i = 0; i < productos.length; i++) {
+        if (productos[i].color == "3,155,229 ,1") {
+            if (arbustoCheck==false) {
+                productosFiltrados.push(productos[i]);
+
+            } else {
+                var index =productosFiltrados.indexOf(productos[i]);
+                if (index >= 0) {
+                    productosFiltrados.splice(index, 1);
+                }
+
+            }
+        }
+    }
+
+    if(!arbustoCheck){
+        arbustoCheck=true;
+    } else {
+        arbustoCheck=false;
+    }
+
+};
+
+exports.pushCategoriaArbol= function () {
+
+    for (i = 0; i < productos.length; i++) {
+        if (productos[i].color == "229,57,53 ,1") {
+
+                 if (arbolCheck==false) {
+                     productosFiltrados.push(productos[i]);
+
+                 } else {
+                     var index =productosFiltrados.indexOf(productos[i]);
+                     if (index >= 0) {
+                         productosFiltrados.splice(index, 1);
+                     }
+
+                 }
+
+
+        }
+    }
+    if(!arbolCheck){
+        arbolCheck=true;
+    } else {
+        arbolCheck=false;
+    }
+
+
+};
+
+exports.pushCategoriaHelecho= function () {
+
+    for (i = 0; i < productos.length; i++) {
+        if (productos[i].color == "109,76,65 ,1") {
+            if (helechoCheck==false) {
+                productosFiltrados.push(productos[i]);
+
+            } else {
+                var index =productosFiltrados.indexOf(productos[i]);
+                if (index >= 0) {
+                    productosFiltrados.splice(index, 1);
+                }
+
+            }
+        }
+    }
+
+    if(!helechoCheck){
+        helechoCheck=true;
+    } else {
+        helechoCheck=false;
+    }
+
 };
--- routes/home.js
+++ routes/home.js
@@ -4,7 +4,42 @@ var productosDB= require('../public/js/DBproductos');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('home', {title: 'Jardin Misterioso',productos:productosDB.getall()});
+   var id= req.query.boton;
+   if (id == "botonRed"){
+       console.log("miau miau red");
+       productosDB.pushCategoriaArbol();
+       console.log(productosDB.getFiltro().length);
+   }
+    if (id == "botonBlue"){
+        console.log("miau miau blue");
+        productosDB.pushCategoriaArbusto();
+        console.log(productosDB.getFiltro().length);
+    }
+
+    if (id == "botonYellow"){
+        console.log("miau miau amarillo");
+        productosDB.pushCategoriaFlor();
+        console.log(productosDB.getFiltro().length);
+    }
+
+    if (id == "botonBrow"){
+        console.log("miau miau cafe");
+        productosDB.pushCategoriaHelecho();
+        console.log(productosDB.getFiltro().length);
+    }
+
+   if (productosDB.getFiltro().length == 0) {
+       console.log("sin filtros");
+       res.render('home', {title: 'Jardin Misterioso', productos: productosDB.getall()});
+
+
+
+   } else {
+       console.log("con filtros");
+       res.render('home', {title: 'Jardin Misterioso', productos: productosDB.getFiltro()});
+
+   }
+
 
 });
 
