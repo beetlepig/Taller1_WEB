3fb1beebd6009e7c0b247e351845c97d9dbaaa8a
sky_k <sky_karlos@hotmail.com>
2017-03-12 17:51:25 -0500

    filtros graficos

---
+++ public/js/main.js
@@ -0,0 +1,51 @@
+var btnArray=[];
+var countArray=[];
+
+window.onload = function(){
+    for (i=0;i<4;i++){
+        btnArray.push(document.getElementById("button"+i));
+    }
+    for (i=0;i<4;i++){
+        countArray.push(0);
+    }
+
+    for (i=0;i<4;i++){
+        if(localStorage["bg-color"+i]){
+            btnArray[i].style.backgroundColor = localStorage.getItem("bg-color"+i);
+
+        }
+    }
+
+
+    for (i=0;i<4;i++){
+        if(localStorage["count"+i]){
+            countArray[i]= localStorage.getItem("count"+i);
+        }
+    }
+
+
+
+    // remove from storage localStorage.removeItem("bg-color")
+};
+
+
+
+function setColor(btn, colorOne, colorTwo){
+
+
+    if (countArray[btn] == 0) {
+        localStorage.removeItem("bg-color"+btn);
+        localStorage.setItem("bg-color"+btn, colorOne);
+        countArray[btn] = 1;
+    }
+    else {
+        localStorage.removeItem("bg-color"+btn);
+        localStorage.setItem("bg-color"+btn, colorTwo);
+        countArray[btn] = 0;
+    }
+    localStorage.setItem("count"+btn, countArray[btn]);
+    // btn.style.backgroundColor = localStorage.getItem("bg-color");
+}
+
+
+
